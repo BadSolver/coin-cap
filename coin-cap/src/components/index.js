d0ca90fbfc6353b196055d1acd3b426f24b9bef5
Alex Kurov <95409553+BadSolver@users.noreply.github.com>
2023-08-29 00:30:11 +0300

    refactor: update barrel pattern

--- coin-cap/src/components/index.js
+++ coin-cap/src/components/index.js
@@ -2,5 +2,6 @@ import { CurrencyItem } from "./CurrencyItem";
 import { CurrencyList } from "./CurrencyList";
 import { Header } from "./Header";
 import { MainTemplate } from "./MainTemplate";
+import { ModalWindow } from "./ModalWindow";
 
-export { Header, CurrencyList, CurrencyItem, MainTemplate };
+export { Header, CurrencyList, CurrencyItem, MainTemplate, ModalWindow };
