c3a86e2b73b7c46f027ad933956b52f5f2dc6178
Alex Kurov <95409553+BadSolver@users.noreply.github.com>
2023-08-29 23:02:47 +0300

    feat: add wallet pages and styles

--- coin-cap/src/pages/WalletPage/WalletPage.jsx
+++ coin-cap/src/pages/WalletPage/WalletPage.jsx
@@ -1,5 +1,22 @@
+// @ts-nocheck
+import { CurrencyList } from "components";
 import React from "react";
+import { useSelector } from "react-redux";
+import "./style.css"; 
 
 export const WalletPage = () => {
-  return <h1>WalletPage</h1>;
+  const wallet = useSelector((state) => state.wallet.wallet);
+  const walletSum = wallet.reduce((acc, item) => acc + item.priceUsd, 0);
+  return (
+    <div>
+      <h1>{wallet.length > 0 ? "Ваш кошелек" : "Ваш кошелек пуст"}</h1>
+      {wallet.length > 0 && (
+        <>
+          <CurrencyList data={wallet} showDeleteButton={true} />
+          <h2>Общая ценность кошелька:</h2>
+          <span className="total-sum">{walletSum} $</span>
+        </>
+      )}
+    </div>
+  );
 };
