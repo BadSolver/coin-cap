112c8fde10c51fa3ee97dbd79858c14f7dbf2b4e
Alex Kurov <95409553+BadSolver@users.noreply.github.com>
2023-08-29 23:03:07 +0300

    feat: update walletSlice

--- coin-cap/src/store/walletSlice.js
+++ coin-cap/src/store/walletSlice.js
@@ -9,14 +9,25 @@ const walletSlice = createSlice({
   },
   reducers: {
     addToWallet(state, { payload }) {
-      state.wallet.push(payload);
+      const findItem = state.wallet.find((item) => item.id === payload.id);
+      if (!findItem) {
+        state.wallet.push(payload);
+      } else {
+        findItem.priceUsd += payload.priceUsd;
+        findItem.count = +payload.count + +findItem.count;
+      }
+      console.log(findItem);
     },
     deleteAllCurrency(state) {
       state.wallet = [];
+    },
+    deleteOneCurrency(state, { payload }) {
+      state.wallet = state.wallet.filter((item) => item.id !== payload);
     },
   },
 });
 
 export default walletSlice.reducer;
 
-export const { addToWallet, deleteAllCurrency } = walletSlice.actions;
+export const { addToWallet, deleteAllCurrency, deleteOneCurrency } =
+  walletSlice.actions;
