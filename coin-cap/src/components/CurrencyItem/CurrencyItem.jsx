733b08426f2935e4b9272ba506102c875cc0d7a7
Alex Kurov <95409553+BadSolver@users.noreply.github.com>
2023-08-30 00:12:35 +0300

    feat: add new func for detail  page

--- coin-cap/src/components/CurrencyItem/CurrencyItem.jsx
+++ coin-cap/src/components/CurrencyItem/CurrencyItem.jsx
@@ -4,6 +4,7 @@ import "./style.css";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { deleteOneCurrency } from "store/walletSlice";
+import { addDetailCurrency } from "store/detailSlice";
 
 export const CurrencyItem = ({
   currency,
@@ -13,19 +14,32 @@ export const CurrencyItem = ({
 }) => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+
+  const handleDetailPage = (selectedCurrency) => {
+    dispatch(addDetailCurrency(selectedCurrency));
+    navigate("/detail");
+  };
+
   const handleDeleteOneCurrency = (id) => {
     dispatch(deleteOneCurrency(id));
   };
+
+  const changePercent = parseFloat(currency.changePercent24Hr);
 
   return (
     <li className="currency-item" key={currency.id}>
       <span>{index + 1}</span>
       <span>{currency.symbol}</span>
-      <span onClick={() => navigate("/detail")} className="currency-name">
+      <span
+        onClick={() => handleDetailPage(currency)}
+        className="currency-name"
+      >
         {currency.name}
       </span>
       <span>{Number(currency.vwap24Hr).toFixed(2)}</span>
-      <span>{Number(currency.changePercent24Hr).toFixed(2)}</span>
+      <span className={`${changePercent > 0 ? "positive" : "negative"}`}>
+        {Number(currency.changePercent24Hr).toFixed(2)} %
+      </span>
       <span>{Number(currency.marketCapUsd).toFixed(2)}</span>
       <span>{Number(currency.priceUsd).toFixed(2)} $</span>
       <span>
