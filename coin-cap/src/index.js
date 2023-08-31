a076ec083417781755c75f32b84137aadb9599e1
Alex Kurov <95409553+BadSolver@users.noreply.github.com>
2023-09-01 00:19:28 +0300

    feat: add redux persist

--- coin-cap/src/index.js
+++ coin-cap/src/index.js
@@ -1,19 +1,19 @@
 // @ts-nocheck
-import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import store from "./store/index";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import store, { persistor } from "./store/index";
+import { HashRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <HashRouter>
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
-  </BrowserRouter>
-  // </HashRouter>
+  </HashRouter>
 );
--- coin-cap/src/store/index.js
+++ coin-cap/src/store/index.js
@@ -1,12 +1,42 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import currencyReducer from "./currencySlice";
 import walletReducer from "./walletSlice";
 import detailReducer from "./detailSlice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-export default configureStore({
-  reducer: {
-    currency: currencyReducer,
-    wallet: walletReducer,
-    detail: detailReducer,
-  },
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  currency: currencyReducer,
+  wallet: walletReducer,
+  detail: detailReducer,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
+
+export default store;
